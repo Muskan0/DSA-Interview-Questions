389401974902dabfcb71af49c8d902a9958bd0e9
Muskan Kalra <49088492+Muskan0@users.noreply.github.com>
2020-10-29 10:55:19 +0530

    Update uniquePaths.py

--- arrays/uniquePaths.py
+++ arrays/uniquePaths.py
@@ -11,3 +11,25 @@ class Solution:
             res*=(l-i)
             res//=i+1
         return res
+# using recursion with memoization
+'''
+class Solution:
+    def uniquePaths(self, m: int, n: int) -> int:
+        dp=[[0 for i in range(n)] for j in range(m)]
+        #print(n)
+        #print(m)
+        #print(dp)
+        return uniquePaths1(0,0,m,n,dp )
+
+def uniquePaths1(i,j,m,n,dp):
+    if (i==m-1) and (j==n-1):
+        return 1
+    if i>=m or j>=n:
+        return 0
+    if dp[i][j]!=0:
+        return dp[i][j]
+    else:
+        dp[i][j]= uniquePaths1(i+1, j, m,n, dp)+uniquePaths1(i,j+1, m,n, dp)
+        return dp[i][j]
+'''
+
