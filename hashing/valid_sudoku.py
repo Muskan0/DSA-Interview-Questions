bac45d12190eaba5999757d60e5a1f9faf00278f
Muskan Kalra <49088492+Muskan0@users.noreply.github.com>
2020-11-24 13:30:08 +0530

    Create valid_sudoku.py

---
+++ hashing/valid_sudoku.py
@@ -0,0 +1,53 @@
+# https://leetcode.com/problems/valid-sudoku/
+
+# optimal approach- one pass
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        
+        rows=[{} for i in range(9)]
+        columns=[{} for i in range(9)]
+        boxes=[{} for i in range(9)]
+        for i in range(9):
+            for j in range(9):
+                if board[i][j]!='.':
+                    num=int(board[i][j])
+                    #print(num)
+                    box_index=(i//3)*3 + (j//3)
+                    if rows[i].get(num,0)==1 or columns[j].get(num,0)==1 or boxes[box_index].get(num,0)==1:
+                        
+                        return False
+                    
+                    rows[i][num]=1
+                    columns[j][num]=1
+                    boxes[box_index][num]=1
+                    #print(rows)
+        return True
+       
+'''
+# naive approach in three passes
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        return self.isValidrow(board) and self.isValidcolumn(board) and self.isValidSquare(board)
+        
+    def isValidunit(self,unit):
+        unit=[i for i in unit if i!="."]
+        return len(unit)==len(set(unit))
+              
+    def isValidrow(self, board):
+        for row in board:
+            if not self.isValidunit(row):
+                return False
+        return True
+    def isValidcolumn(self, board):
+        for column in zip(*board):
+            if not self.isValidunit(column):
+                return False
+        return True
+    def isValidSquare(self,board):
+        for i in (0,3,6):
+            for j in (0,3,6):
+                square=[board[x][y] for x in range(i,i+3) for y in range(j,j+3)]
+                if not self.isValidunit(square):
+                    return False
+        return True
+'''
