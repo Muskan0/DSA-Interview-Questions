4d3e8eae2af7a8db9e44dd15b753bc620fcaf609
Muskan Kalra <49088492+Muskan0@users.noreply.github.com>
2020-11-24 13:35:52 +0530

    Update valid_sudoku.py

--- hashing/valid_sudoku.py
+++ hashing/valid_sudoku.py
@@ -22,6 +22,29 @@ class Solution:
                     boxes[box_index][num]=1
                     #print(rows)
         return True
+'''
+# approach using single set
+class Solution:
+    def isValidSudoku(self, board: List[List[str]]) -> bool:
+        s=set()
+        for i in range(9):
+            for j in range(9):
+                if board[i][j]!='.':
+                    r="R"+str(i)+board[i][j]
+                    if r in s:
+                        return False
+                    s.add(r)
+                    c="C"+str(j)+board[i][j]
+                    if c in s:
+                        return False
+                    s.add(c)
+                    box=(i // 3 ) * 3 + j // 3
+                    b="B"+str(box)+board[i][j]
+                    if b in s:
+                        return False
+                    s.add(b)
+        return True
+'''
        
 '''
 # naive approach in three passes
