1aa2f9dc1ad842fb2778b9fe4db7344720a6a075
Muskan Kalra <49088492+Muskan0@users.noreply.github.com>
2020-11-30 18:14:23 +0530

    Update nth_root_of_m.py

--- binary search/nth_root_of_m.py
+++ binary search/nth_root_of_m.py
@@ -5,7 +5,10 @@ for _ in range(int(input())):
     low=1
     high=m
     guess=(low+high)/2
-    epsilon=0.0000001
+    
+    epsilon=0.0000001 # for taking approximations
+    
+    # binary search
     while abs(guess**n-m)>=epsilon:
         if guess**n>m:
             high=guess
@@ -15,5 +18,7 @@ for _ in range(int(input())):
     #print(round(guess))
     if m==round(guess)**n:
         print(int(round(guess)))
+       # The round() function returns a floating point number that is a rounded version of the specified number, with the specified number of decimals.
+       # The default number of decimals is 0, meaning that the function will return the nearest integer.
     else:
         print(-1)
