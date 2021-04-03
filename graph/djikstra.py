dc6cc9a6efb8bbb8261736fb0d90a7d23e053821
Muskan Kalra <49088492+Muskan0@users.noreply.github.com>
2021-04-03 21:51:54 +0530

    Update djikstra.py

--- graph/djikstra.py
+++ graph/djikstra.py
@@ -46,3 +46,46 @@ while processed!=V:
     processed.add(pr)
 print(path)
 # print(time.time()-start)
+
+
+
+########################################################################################
+
+# using heap
+# https://leetcode.com/problems/network-delay-time/
+
+class Solution:
+    def networkDelayTime(self, times: List[List[int]], n: int, k: int) -> int:
+        import heapq
+        def dijkstra(graph, V):
+            d=[float("inf") for i in range(V+1)]
+            d[k]=0
+            l=[[0]]
+            l[0].append(k)
+            while l:
+                temp=heapq.heappop(l)
+                u=temp[1]
+                for i in graph[u]:
+                    v=i[0]
+                    w=i[1]
+                    if d[v]> d[u]+w:
+                        if d[v]!=float('inf'):
+                            if [d[v],v] in l:
+                                l.remove([d[v],v])
+                        d[v]=d[u]+w
+                        l.append([d[v],v])
+           
+            for i in d[1:]:
+                if i==float('inf'):
+                    return -1
+            return max(d[1:])
+
+        
+        di={i:[] for i in range(1,n+1)}
+        for i in times:
+            u,v,w=i[0],i[1],i[2]
+            di[u].append([v,w])
+
+        return dijkstra(di,n)
+# time complexity: O(ElogV)
+# space complexity: E+V + V, E+V is to store the graph and V is worst case space complexity for heap= O(2*V + E)
