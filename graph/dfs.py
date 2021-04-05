2b8ac06a255accc7abc9ee91d2cd6f5849c47cad
Muskan Kalra <49088492+Muskan0@users.noreply.github.com>
2021-04-05 19:54:19 +0530

    Update dfs.py

--- graph/dfs.py
+++ graph/dfs.py
@@ -19,6 +19,39 @@ def dfs(g,N):
     visited=[False for i in range(N)]
     dfsUtil(g,0,visited)
     return result
-
-
 # TAKUFORWARD DFS video: https://youtu.be/_GstINcASBI
+
+
+
+############################################
+# dfs using stack
+# https://www.hackerearth.com/practice/algorithms/graphs/depth-first-search/tutorial/
+
+def dfs(V,g,x):
+        
+        result=[]
+        visited=[False for i in range(V+1)]        
+        st=[]
+        st.append(x)
+
+        while st:
+            node=st[-1]
+            st.pop(-1)
+            if not visited[node]:
+                result.append(node)
+                visited[node]=True
+            for i in g[node]:
+
+                if not visited[i]:
+                    st.append(i)
+        return len(result)
+
+n,m=list(map(int, input().split()))
+g={i:[] for i in range(1,n+1)}
+for i in range(m):
+    a,b=map(int, input().split())
+    g[a].append(b)
+    g[b].append(a)
+    
+x=int(input())
+print(abs(dfs(n,g,x)-n))
