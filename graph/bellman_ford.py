660f11e6031b0467b44dddf4ab5cc110afe0cc84
Muskan Kalra <49088492+Muskan0@users.noreply.github.com>
2021-04-08 22:02:34 +0530

    Update bellman_ford.py

--- graph/bellman_ford.py
+++ graph/bellman_ford.py
@@ -2,19 +2,19 @@
 
 class Solution:
 	def isNegativeWeightCycle(self, n, g):
-		d=[float('inf') for i in range(n+1)]
-		d[1]=0
-		for i in range(n-1):
-				for u,v,w in g:
-
-			if d[u]+w<d[v]:
-					d[v]=d[u]+w
-
-		for u,v,w in g:
-				if d[u]+w<d[v]:
-			return 1
-
-		return 0
+	    d=[float('inf') for i in range(n+1)]
+        d[1]=0
+        for i in range(n-1):
+            for u,v,w in g:
+                
+                if d[u]+w<d[v]:
+                    d[v]=d[u]+w
+        
+        for u,v,w in g:
+            if  d[u]+w<d[v]:
+                return 1
+        
+        return 0
 
 # time complexity: O(NE)
 # space complexity: O(N+E) for storing adjacency list
