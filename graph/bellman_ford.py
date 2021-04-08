b111a5fc1dcf244ea4657d41744ef143a1c1f34f
Muskan Kalra <49088492+Muskan0@users.noreply.github.com>
2021-04-08 23:03:48 +0530

    Update bellman_ford.py
    Corrected the indentation

--- graph/bellman_ford.py
+++ graph/bellman_ford.py
@@ -1,8 +1,8 @@
 # https://practice.geeksforgeeks.org/problems/negative-weight-cycle3504/1#
 
 class Solution:
-	def isNegativeWeightCycle(self, n, g):
-	    d=[float('inf') for i in range(n+1)]
+    def isNegativeWeightCycle(self, n, g):
+        d=[float('inf') for i in range(n+1)]
         d[1]=0
         for i in range(n-1):
             for u,v,w in g:
@@ -15,6 +15,6 @@ class Solution:
                 return 1
         
         return 0
-
+        
 # time complexity: O(NE)
 # space complexity: O(N+E) for storing adjacency list
