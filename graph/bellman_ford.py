f2616882baa52fb96f2cace8d558505316f0ff06
Muskan Kalra <49088492+Muskan0@users.noreply.github.com>
2021-04-08 16:02:28 +0530

    Create bellman_ford.py

---
+++ graph/bellman_ford.py
@@ -0,0 +1,19 @@
+# https://practice.geeksforgeeks.org/problems/negative-weight-cycle3504/1#
+
+class Solution:
+	def isNegativeWeightCycle(self, n, g):
+	    d=[float('inf') for i in range(n+1)]
+        d[1]=0
+        for i in range(n-1):
+            for u,v,w in g:
+                
+                if d[u]+w<d[v]:
+                    d[v]=d[u]+w
+        
+        for u,v,w in g:
+            if d[u]+w<d[v]:
+                return 1
+        
+        return 0
+
+ # time complexity: O(NE)
